bde7c8c54f87585a18233928955bf9aabdfac23f
Mushan Yang <mushanyoung@gmail.com>
2019-09-01 06:00:04 +0000

    fix: dht paths

--- start.sh
+++ start.sh
@@ -19,8 +19,8 @@ touch /conf/dht6.dat
 chown $PUID:$PGID /conf/aria2.conf
 chown $PUID:$PGID /conf/aria2.session
 chown $PUID:$PGID /conf/aria2-watching.log
-chown $PUID:$PGID /dht.dat
-chown $PUID:$PGID /dht6.dat
+chown $PUID:$PGID /conf/dht.dat
+chown $PUID:$PGID /conf/dht6.dat
 
 s6-setuidgid $PUID:$PGID inotifywait -m -e create -e moved_to --format '%w%f' '/watch' | s6-setuidgid $PUID:$PGID xargs -n1 -I{} sh -c 'echo "{}" | grep "\.torrent$"; if [ $? -eq 0 ]; then /aria2-watching/diana add "{}" && mv "{}" "{}.added"; fi' &
 
