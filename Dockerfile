b47cd920a7c162b53d2384b262909ef7c7e587df
Mushan Yang <mushanyoung@gmail.com>
2019-09-01 20:14:53 +0000

    feat: simplify Dockerfile and start.sh

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,19 @@ FROM alpine:edge
 
 MAINTAINER mushanyoung <mushanyoung@gmail.com>
 
+ADD aria2.conf start.sh https://raw.githubusercontent.com/baskerville/diana/master/diana /aria2-watching/
+
 RUN mkdir -p /conf /data /watch \
-             /aria2-watching /aria2-watching/ui
-
-ADD aria2.conf /aria2-watching/aria2.conf
-ADD start.sh /aria2-watching/start.sh
-RUN chmod +x /aria2-watching/start.sh
-
-RUN apk update && apk add --no-cache aria2 darkhttpd s6 inotify-tools python3
-
-RUN apk add --no-cache --virtual .install-deps curl jq unzip \
+             /aria2-watching /aria2-watching/ui \
+ && chmod +x /aria2-watching/diana /aria2-watching/start.sh \
+ && apk add --no-cache s6 aria2 darkhttpd inotify-tools python3 \
+ && apk add --no-cache --virtual .install-deps curl jq unzip \
  && curl -o /aria2ng.zip -L $(curl -sX GET 'https://api.github.com/repos/mayswind/AriaNg/releases/latest' | jq -r '.assets[0].browser_download_url') \
  && unzip /aria2ng.zip -d /aria2-watching/ui \
  && rm /aria2ng.zip \
- && curl 'https://raw.githubusercontent.com/baskerville/diana/master/diana' -o /aria2-watching/diana \
- && chmod +x /aria2-watching/diana \
  && apk del .install-deps
 
 VOLUME /conf /data /watch
 EXPOSE 6800/tcp 8080/tcp
 
-CMD /aria2-watching/start.sh
+CMD ["/aria2-watching/start.sh"]
--- start.sh
+++ start.sh
@@ -3,28 +3,18 @@ set -e
 
 export PUID=${PUID:-0}
 export PGID=${PGID:-0}
-export SECRET=${SECRET:-watching}
-export DIANA_SECRET_TOKEN=$SECRET
+export SECRET="${SECRET:-watching}"
+export DIANA_SECRET_TOKEN="${SECRET}"
 
-if [ ! -f /conf/aria2.conf ]; then
-  cp /aria2-watching/aria2.conf /conf/aria2.conf
-  echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
-fi
+[ -f /conf/aria2.conf ] || cp /aria2-watching/aria2.conf /conf/aria2.conf
+grep '^rpc-secret=' /conf/aria2.conf &>/dev/null || echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
 
-touch /conf/aria2.session
-touch /conf/aria2-watching.log
-touch /conf/dht.dat
-touch /conf/dht6.dat
+touch /conf/aria2.session /conf/aria2c.log /conf/darkhttpd.log /conf/dht.dat /conf/dht6.dat
+chown $PUID:$PGID /conf/aria2.conf /conf/aria2.session /conf/aria2c.log /conf/darkhttpd.log /conf/dht.dat /conf/dht6.dat
 
-chown $PUID:$PGID /conf/aria2.conf
-chown $PUID:$PGID /conf/aria2.session
-chown $PUID:$PGID /conf/aria2-watching.log
-chown $PUID:$PGID /conf/dht.dat
-chown $PUID:$PGID /conf/dht6.dat
+s6-setuidgid $PUID:$PGID darkhttpd /aria2-watching/ui --daemon --port 8080 --log /conf/darkhttpd.log
+
+s6-setuidgid $PUID:$PGID aria2c -D --conf-path /conf/aria2.conf --log /conf/aria2c.log
 
 s6-setuidgid $PUID:$PGID inotifywait -m -e create -e moved_to --format '%w%f' '/watch' | \
-s6-setuidgid $PUID:$PGID xargs -n1 -I{} sh -c 'echo "{}" | grep "\.torrent$"; if [ $? -eq 0 ]; then /aria2-watching/diana add "{}" && mv "{}" "{}.added"; fi' &
-
-s6-setuidgid $PUID:$PGID darkhttpd /aria2-watching/ui --port 8080 &
-
-exec s6-setuidgid $PUID:$PGID aria2c --conf-path=/conf/aria2.conf --log=/conf/aria2-watching.log
+s6-setuidgid $PUID:$PGID xargs -n 1 -I {} sh -c 'echo "{}" | grep "\.torrent$"; if [ $? -eq 0 ]; then /aria2-watching/diana add "{}" && mv "{}" "{}.added"; fi' &
