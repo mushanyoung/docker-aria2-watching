77c8f86e353203c7e8e47335dcebac371cd1f4ec
Mushan Yang <mushanyoung@gmail.com>
2019-09-01 05:34:35 +0000

    fix: watching job runs in background

--- start.sh
+++ start.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 set -e
 
-PUID=${PUID:-0}
-PGID=${PGID:-0}
-SECRET=${SECRET:-watching}
+export PUID=${PUID:-0}
+export PGID=${PGID:-0}
+export SECRET=${SECRET:-watching}
+export DIANA_SECRET_TOKEN=$SECRET
 
 if [ ! -f /conf/aria2.conf ]; then
   cp /aria2-watching/aria2.conf /conf/aria2.conf
-  chown $PUID:$PGID /conf/aria2.conf
   echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
 fi
 
 touch /conf/aria2.session
-chown $PUID:$PGID /conf/aria2.session
-
 touch /logs.txt
+
+chown $PUID:$PGID /conf/aria2.conf
+chown $PUID:$PGID /conf/aria2.session
 chown $PUID:$PGID /logs.txt
 
-export DIANA_SECRET_TOKEN=$SECRET
-s6-setuidgid $PUID:$PGID inotifywait -m -e create -e moved_to --format '%w%f' '/watch' | xargs -n1 -I{} sh -c 'echo "{}" | grep "\.torrent$"; if [ $? -eq 0 ]; then /aria2-watching/diana add "{}" && mv "{}" "{}.added"; fi'
+s6-setuidgid $PUID:$PGID inotifywait -m -e create -e moved_to --format '%w%f' '/watch' | xargs -n1 -I{} sh -c 'echo "{}" | grep "\.torrent$"; if [ $? -eq 0 ]; then /aria2-watching/diana add "{}" && mv "{}" "{}.added"; fi' &
 
 s6-setuidgid $PUID:$PGID darkhttpd /aria2-watching/ui --port 8080 &
 
