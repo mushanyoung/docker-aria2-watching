38409ad17d71eb9f23e49c260481972e277046d5
Mushan Yang <mushanyoung@gmail.com>
2019-09-01 05:53:33 +0000

    fix: saves log to /conf so that it can be easier to inspect outside docker
    fix: dht paths

--- start.sh
+++ start.sh
@@ -12,14 +12,18 @@ if [ ! -f /conf/aria2.conf ]; then
 fi
 
 touch /conf/aria2.session
-touch /logs.txt
+touch /conf/aria2-watching.log
+touch /conf/dht.dat
+touch /conf/dht6.dat
 
 chown $PUID:$PGID /conf/aria2.conf
 chown $PUID:$PGID /conf/aria2.session
-chown $PUID:$PGID /logs.txt
+chown $PUID:$PGID /conf/aria2-watching.log
+chown $PUID:$PGID /dht.dat
+chown $PUID:$PGID /dht6.dat
 
-s6-setuidgid $PUID:$PGID inotifywait -m -e create -e moved_to --format '%w%f' '/watch' | xargs -n1 -I{} sh -c 'echo "{}" | grep "\.torrent$"; if [ $? -eq 0 ]; then /aria2-watching/diana add "{}" && mv "{}" "{}.added"; fi' &
+s6-setuidgid $PUID:$PGID inotifywait -m -e create -e moved_to --format '%w%f' '/watch' | s6-setuidgid $PUID:$PGID xargs -n1 -I{} sh -c 'echo "{}" | grep "\.torrent$"; if [ $? -eq 0 ]; then /aria2-watching/diana add "{}" && mv "{}" "{}.added"; fi' &
 
 s6-setuidgid $PUID:$PGID darkhttpd /aria2-watching/ui --port 8080 &
 
-exec s6-setuidgid $PUID:$PGID aria2c --conf-path=/conf/aria2.conf --log=/logs.txt
+exec s6-setuidgid $PUID:$PGID aria2c --conf-path=/conf/aria2.conf --log=/conf/aria2-watching.log
